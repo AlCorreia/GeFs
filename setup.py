aa9f5d656bd20ce13bea0e0beedcedcd2b7256ff
AlCorreia <alvaro.hc.correia@gmail.com>
2021-02-10 18:17:57 +0100

    Requiring scipy version to be 1.5 or higher

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='gefs',
       author_email='a.h.chaim.correia@tue.nl',
       license='MIT',
       packages=['gefs'],
-      install_requires=['numpy', 'numba', 'pandas', 'scipy', 'sklearn', 'tqdm'],
+      install_requires=['numpy', 'numba', 'pandas', 'scipy>=1.5', 'sklearn', 'tqdm'],
       zip_safe=False)
